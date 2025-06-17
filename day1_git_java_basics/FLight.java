c427586f2060dfbfc1d4ab1736364d9d8e2e9624
Visali-BRS <visalinalluri0504@gmail.com>
2025-06-17 16:08:51 +0530

    Notes updated

---
+++ day1_git_java_basics/FLight.java
@@ -0,0 +1,10 @@
+
+import java.util.Objects;
+
+public class Flight {
+	private int id;
+	private String airline,source,destination;
+	private float fare,duration;
+	public Flight() {
+		super();
+	}
